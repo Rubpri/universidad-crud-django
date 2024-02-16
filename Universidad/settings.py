664dbe1f6f8be65f4f899027eaeb955d5cff297b
Rubpri <rubenprietoserrano@gmail.com>
2024-02-16 13:40:22 +0100

    prueba deploy

--- Universidad/settings.py
+++ Universidad/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'vercel.app', '*.vercel.app']
 
 
 
